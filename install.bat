393b512f5354b25b23121441610ed6ad43adb318
Midrooms <108638658+Midrooms@users.noreply.github.com>
2022-10-10 01:55:43 -0400

    Update install.bat
    Small updates

--- install.bat
+++ install.bat
@@ -1,13 +1,11 @@
-@echo Please wait while CrepeSR is installed...
-
 powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/983998821786878002/1026344036728061982/QuickServerLaunch.bat -Outfile QuickServerLaunch.bat"
 powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/983998821786878002/1026344793527304212/temp.bat -Outfile temp.bat"
 START temp.bat
 cls
-@echo You must wait for NPM to install. (It's the window that opened when you ran this) Once that's done, close that window and press any key to continue.
-
+@echo The window that just opened is the NPM installation.
+@echo Please wait for that installatkon to finish. Close that after, then press any key to continue.
 pause
-
 START QuickServerLaunch.bat
 DEL temp.bat
+DEL readme.md
 DEL install.bat
