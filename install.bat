3a82a3027ac9da79ebe3c7e784a357a696801647
Midrooms <108638658+Midrooms@users.noreply.github.com>
2022-10-17 12:47:04 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,10 +1,8 @@
 powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/983998821786878002/1026344036728061982/QuickServerLaunch.bat -Outfile QuickServerLaunch.bat"
 powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/983998821786878002/1026344793527304212/temp.bat -Outfile temp.bat"
-:: powershell -Command "Expand-Archive -LiteralPath <path> -DestinationPath <path>" I think this is right
 START temp.bat
 cls
-@echo The window that just opened is the NPM installation.
-@echo Please wait for that installatkon to finish. Close that after, then press any key to continue.
+@echo Please wait for NPM to be installed in the window that just opened. Close it once it's finished, then press a key to continue.
 pause
 START QuickServerLaunch.bat
 DEL LICENSE
