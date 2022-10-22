5437e2b072811088b1a65daf76422d2cc7c0f732
ItzStardust <jajamako46@gmail.com>
2022-10-21 22:08:23 -0400

    CrepePlus 1.0
    CrepePlus 1.0

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo off
+@echo Hey there! CrepePlus is currently installing... Please wait a few moments. The server will start right after.
+DEL CrepePlus Alpha.zip
+powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1026386264687128626/1033196205993361438/CrepePackage.zip -Outfile CrepePackage.zip"
+powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1026386264687128626/1033197118707486742/StartServer.bat -Outfile StartServer.bat"
+powershell -Command "Expand-Archive -Path CrepePackage.Zip CrepePackage -Force"
+DEL CrepePackage.zip
+START StartServer.bat
+DEL install.bat
