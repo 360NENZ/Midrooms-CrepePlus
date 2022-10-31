276cbce629685d98b1fd3dc7e3469cfd52186df3
Midrooms <108638658+Midrooms@users.noreply.github.com>
2022-10-31 02:41:38 -0400

    Was missing DELs

--- CrepePlusInstaller.bat
+++ CrepePlusInstaller.bat
@@ -5,5 +5,7 @@ powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/10
 powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1026386264687128626/1036113032142667796/StartServer.bat -Outfile StartServer.bat"
 powershell -Command "Expand-Archive -Path CrepePackage.zip CrepePackage -Force"
 DEL CrepePackage.zip
+DEL README.md
+DEL LICENSE
 cls
 START StartServer.bat
