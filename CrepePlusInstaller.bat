422d89c5619d3f2ce11daf26c9402d79fc3b6a5e
ItzStardust <jajamako46@gmail.com>
2022-11-06 23:59:22 -0500

    Update installer
    The package will always be up to date, no need for installer updates every time there is a new package.

--- CrepePlusInstaller.bat
+++ CrepePlusInstaller.bat
@@ -1,7 +1,7 @@
 @echo Hey there! CrepePlus is currently installing... Please wait a few moments. The server will start right after.
 @echo In the meantime, please open MongoDB and press the green 'Connect' button!
 @echo off
-powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1026386264687128626/1036114351934931034/CrepePackage.zip -Outfile CrepePackage.zip
+powershell -Command "Invoke-WebRequest https://github.com/Midrooms/CrepePackages/raw/main/CrepePackage.zip -Outfile CrepePackage.zip
 powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1026386264687128626/1036113032142667796/StartServer.bat -Outfile StartServer.bat"
 powershell -Command "Expand-Archive -Path CrepePackage.zip CrepePackage -Force"
 DEL CrepePackage.zip
