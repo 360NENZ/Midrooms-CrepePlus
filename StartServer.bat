72a40d9aaee104719e7df6a98ca46598aa396aea
Midrooms <jajamako46@gmail.com>
2022-12-08 06:54:03 -0500

    Version 1.4
    Replaces old files with a new "CrepePlus Manager"

--- StartServer.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd %cd%\CrepePackage\
-START MITMStart.bat
-net start MongoDB
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d http://127.0.0.1:8080 /f
-npm run start
